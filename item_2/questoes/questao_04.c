1426700a4bf57bfc6482c1564d92e154af4eadff
Daniel Rodrigues <61955775+dxnielsr@users.noreply.github.com>
2020-11-25 20:35:12 -0400

    questao_04 done

---
+++ item_2/questoes/questao_04.c
@@ -0,0 +1,13 @@
+// Escreva um algoritmo que imprima
+// os 10 primeiros números maiores que 100
+
+#include <stdio.h>
+
+int main() {
+    
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n", 100 + i);
+    }
+
+    return 0;
+}
