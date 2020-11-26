efa8d722ce132688477b63aa92f814876428efc7
Daniel Rodrigues <61955775+dxnielsr@users.noreply.github.com>
2020-11-26 16:19:14 -0400

    Update questao_07.c

--- item_2/questoes/questao_07.c
+++ item_2/questoes/questao_07.c
@@ -1,3 +1,21 @@
 // Faça um algoritmo que leia 5 números e escrever
 // quantos estão no intervalo de 10 a 20
 // e quantos estão fora do intervalo
+
+#include <stdio.h>
+
+int main() {
+    int nums = 0, i, qtd = 0;
+
+    for(i = 0; i < 5; i++) {
+        printf("digite um numero\n");
+        scanf("%d", &nums);
+        if(nums >= 10 && nums <= 20) {
+            qtd++;
+        }
+    }
+
+    printf("%d no intervalo", qtd);
+
+    return 0;
+}
