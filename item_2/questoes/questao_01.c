cce1dbcd2554391565e24bade485e6222558081f
Daniel Rodrigues <61955775+dxnielsr@users.noreply.github.com>
2020-11-25 20:25:42 -0400

    questoes repetidas copypast

---
+++ item_2/questoes/questao_01.c
@@ -0,0 +1,13 @@
+// Faça um algoritmo para escrever os números de 1 a 30 em ordem
+// crescente
+
+#include <stdio.h>
+
+int main() {
+    
+    for(int i = 1; i <= 30; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0; 
+}
