468262ce14a1b080d994285d74fa58f1890d896e
Daniel Rodrigues <61955775+dxnielsr@users.noreply.github.com>
2020-11-25 20:17:40 -0400

    questao_07 done

---
+++ item_1/questoes/questao_07.c
@@ -0,0 +1,13 @@
+// Faça um algoritmo para escrever os números de 1 a 20 em ordem
+// decrescente:
+
+#include <stdio.h>
+
+int main() {
+
+    for(int i = 20; i >= 1; i--) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
