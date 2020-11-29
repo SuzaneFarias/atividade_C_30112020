c3645f02bc9534299e7e8059b0d9229cc36555e3
Daniel Rodrigues <61955775+dxnielsr@users.noreply.github.com>
2020-11-29 15:27:05 -0400

    exemplo copiado

---
+++ item_5/exemplo/ex_01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    
+    int mat[2][2];
+    float det;
+    int x,y;
+
+    printf("Este programa calcula a determinante de uma matriz quadrada de ordem 2");
+    printf("\n\nEntre com os valores a da matriz:\n");
+
+    for(x=0;x<2;x++){
+        for(y=0;y<2;y++){
+            printf("mat[%d][%d]=",x+1,y+1);
+            scanf("%d",&mat[x][y]);
+            det = mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0];
+        } /*fim do for*/
+    } /*fim do for*/
+
+    printf("Soma: %.2f", det);
+
+    return 0;
+}
